79bf577910eb6be80bb33ee6a56a6a98fb861ac8
Imed Eddine Benzarti <i.benzarti@root.sa>
2018-02-27 14:51:51 +0100

    calculation

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,11 +11,95 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World !!");
-            Console.WriteLine("Hi All !!!");
-            Console.WriteLine("Imed Eddine Benzarti");
-            
-            Console.ReadLine();
+            int x, y;
+
+            Console.WriteLine("----------------  Calculation ------------------------");
+            Console.WriteLine("1) Add");
+            Console.WriteLine("2) Minus");
+            Console.WriteLine("3) Div ");
+            Console.WriteLine("4) Modulo");
+            int Choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (Choice)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("X :");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Y :");
+                        y = Convert.ToInt32(Console.ReadLine());
+                        int z = x + y;
+                        Console.WriteLine("Z : " + z);
+                        Console.ReadKey();
+                        break;
+                    }
+
+                case 2:
+                    {
+                        Console.WriteLine("X :");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Y :");
+                        y = Convert.ToInt32(Console.ReadLine());
+                        int r = x + y;
+                        Console.WriteLine("r : " + r);
+                        Console.ReadKey();
+                        break;
+                    }
+
+                case 3:
+                    {
+                        Console.WriteLine("X :");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Y :");
+                        y = Convert.ToInt32(Console.ReadLine());
+
+                        if (y == 0)
+                        {
+                            Console.WriteLine("ERROR : VARIABLE Y SHOULD BE DIFFERENT THAN 0");
+                        }
+                        else
+                        {
+                            int d = x / y;
+                            Console.WriteLine("d : " + d);
+                        }
+                        Console.ReadKey();
+                    }
+                    break;
+
+                case 4:
+                    {
+                        Console.WriteLine("X :");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Y :");
+                        y = Convert.ToInt32(Console.ReadLine());
+
+                        if (y == 0)
+                        {
+                            Console.WriteLine("ERROR : VARIABLE Y SHOULD BE DIFFERENT THAN 0");
+                        }
+                        else
+                        {
+                            int m = x % y;
+                            Console.WriteLine("m : " + m);
+                        }
+
+                        Console.ReadKey();
+                    }
+                    break;
+
+                default:
+                    {
+                        Console.WriteLine("ADD REAL VARIABLES");
+                    }
+                    break;
+
+            }
+
+
+
+
+
+
 
         }
     }
